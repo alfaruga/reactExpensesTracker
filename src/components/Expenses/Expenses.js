1443aec2fc3e976a0ae77268a08cbae222cbae86
alexisRuiz <alfaruga@gmail.com>
2022-05-17 12:49:32 -0500

    adding dynamic styles and a [Chart], [ChartBar] and [ExpensesChart] elements for visualization of expenses

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,21 +1,23 @@
 import { useState } from "react";
 import ExpensesFilter from "./ExpensesFilter";
 import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
 import "./Expenses.css";
 
 const Expenses = (props) => {
-
   const [year, setYear] = useState(NaN);
 
   const filterFunction = (year) => {
     setYear(parseInt(year));
   };
 
-  
+  let  expenses = props.expensesData.filter(item=>(isNaN(year) || item["date"].getFullYear() === year));
+
   return (
     <div className="expenses">
       <ExpensesFilter onFiltered={filterFunction} />
-      <ExpensesList year={year} expData={props.expensesData}/>
+      <ExpensesChart expenses={expenses} />
+      <ExpensesList year={year} expData={props.expensesData} />
     </div>
   );
 };
